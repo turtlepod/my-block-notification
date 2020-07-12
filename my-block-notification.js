2ef238e36efb749826ecc1cbee62504e287769d5
david <david.chandra@10up.com>
2020-07-12 14:42:50 +0700

    Init.

---
+++ my-block-notification.js
@@ -0,0 +1,47 @@
+const {
+	blocks: { registerBlockType },
+	blockEditor: { RichText },
+} = wp;
+
+registerBlockType( 'my-block/notification', {
+	title: 'My Notification',
+	icon: 'info',
+	category: 'formatting',
+	attributes: {
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: 'p'
+		},
+	},
+	edit: ( props ) => {
+		const { attributes: { content }, setAttributes } = props;
+		const style = {
+			background: '#1e73be',
+			color: '#fff',
+			padding: '5px 45px',
+		};
+		return (
+			<div style={ style }>
+				<RichText
+					tagName="p"
+					onChange={ (newContent) => {
+						setAttributes( { content: newContent } );
+					} }
+					value={ content }
+					placeholder={ 'Tulis info disini' }
+				/>
+			</div>
+		);
+	},
+	save: (props) => {
+		return (
+			<div>
+				<RichText.Content
+					tagName="p"
+					value={ props.attributes.content }
+				/>
+			</div>
+		);
+	},
+} );
---
+++ my-block-notification.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: My Notification
+ */
+
+add_action( 'enqueue_block_editor_assets', function() {
+	wp_enqueue_script(
+		'my-block-notification',
+		trailingslashit( plugin_dir_url( __FILE__ ) ) . 'my-block-notification.min.js',
+		[
+			'wp-element',
+			'wp-blocks',
+			'wp-components',
+			'wp-editor',
+		],
+		'0.1.0',
+		true
+	);
+} );
+
+add_action( 'wp_head', function() {
+?>
+<style>
+.wp-block-my-block-notification {
+	background: #1e73be;
+	color: #fff;
+	padding: 30px 45px 10px 45px; 
+}
+</style>
+<?php
+} );
